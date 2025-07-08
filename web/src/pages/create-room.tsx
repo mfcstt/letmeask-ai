48acaa7c6ba6061bd577c9bfa42130961ffdfebb
Maria Fernanda Rizzo <mahcosta54@gmail.com>
2025-07-08 18:51:07 -0300

    Add web folder as normal folder

---
+++ web/src/pages/create-room.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+export function CreateRoom() {
+    return (
+        <div>
+           <Link to="/room">
+                Acess Room
+              </Link>
+        </div>
+    )
+}
---
+++ web/src/pages/room.tsx
@@ -0,0 +1,7 @@
+export function Room() {
+    return (
+        <div>
+            Room details
+        </div>
+    )
+}
